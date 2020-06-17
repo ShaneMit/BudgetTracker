57e4ae2f7ad88d7cd600faf2d6b6fe9b0ab24da5
ShaneMit <shanemit@cox.net>
2020-06-16 22:03:42 -0700

    created service worker

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('transactions-cache-v1')
+  .then(cache => cache.addAll([
+    '/',
+    '/icons'
+    '/index.html',
+    '/index.js',
+    '/manifest.json',
+    '/styles.css'
+  ])))
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request)
+  .catch(err => {
+    return caches.match(event.request)
+    .then(match => {
+      if (match) {
+        return match
+      } else if (event.request.headers.get('accept').includes('text/html')) {
+        return caches.match('/')
+      }
+    })
+  }))
+})

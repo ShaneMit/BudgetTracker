9a15be8cf0aa1eaef2f9f3a5fd17b46523a4ed7b
ShaneMit <shanemit@cox.net>
2020-06-17 21:38:42 -0700

    Deployed

---
+++ config/index.js
@@ -0,0 +1,1 @@
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI)
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
